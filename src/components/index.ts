e7201a2180e0520984e9e991a6e564368349c069
m3klen <fivemrex9873@gmail.com>
2024-09-27 18:47:38 +0200

    Updated file to progress all components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,9 @@
 import UI from './ui'
+import Header from './header'
+import Login from './auth/Login'
+import Register from './auth/Register'
 
-export { UI }
+import AuthGuard from './auth/AuthGuard/AuthGuard'
+import { useAuth, AuthProvider } from './auth/AuthContext/AuthContext'
+
+export { UI, Header, Login, useAuth, AuthProvider, AuthGuard, Register }
